3422ef985f0f49f5e043524b9afd67e1be7b595e
Alfonso Cabeza <alfonsocabeza34490@gmail.com>
2022-10-07 12:17:30 +0200

    removi la primera linea, y añadi mi grupo favorito

--- Untitled.java
+++ Untitled.java
@@ -1,9 +1,9 @@
 public class Untitled{
 
     public static void main(String args[]) {
-      System.out.println("Cabeza Cuervo");
       System.out.println("18 años 2004");
       System.out.println("Mi comida favorita es la hamburguesa");
+      System.out.println("mi grupo favorito es R.I.P o eskorbuto");
     }
 
 }
